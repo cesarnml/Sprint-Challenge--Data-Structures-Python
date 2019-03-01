76aac0c58f889e8d9e13ed0d1b3b7ecaa16d18d8
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-01 09:23:09 -0800

    improves names.py to under ~.01s

--- names/names.py
+++ names/names.py
@@ -9,11 +9,12 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+# create a dictionary of names_2 where elements are keys and True is value
+name_2_dic = dict((name, True) for name in names_2)
 
 duplicates = []
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+        if name_1 in name_2_dic:
             duplicates.append(name_1)
 
 end_time = time.time()
